e264f71a00b7df9ac32f5adddcf3486214afc8d0
Yuri Ratkevich <ura_rat@mail.ru>
2023-02-09 16:05:14 +0300

    Some commands rework, add photo methods to core

--- Services/BotService.cs
+++ Services/BotService.cs
@@ -9,6 +9,8 @@ public interface IBotService
     BotClient BotClient { get; set; }
     bool SendMessage(SendMessageArgs sendMessageArgs);
     Task<bool> SendMessageAsync(SendMessageArgs sendMessageArgs);
+    bool SendPhoto(SendPhotoArgs sendPhotoArgs);
+    Task<bool> SendPhotoAsync(SendPhotoArgs sendPhotoArgs);
 }
 
 public class BotService : IBotService
@@ -40,6 +42,34 @@ public class BotService : IBotService
         try
         {
             await this.BotClient.SendMessageAsync(sendMessageArgs);
+            return true;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return false;
+        }
+    }
+    
+    public bool SendPhoto(SendPhotoArgs sendPhotoArgs)
+    {
+        try
+        {
+            this.BotClient.SendPhoto(sendPhotoArgs);
+            return true;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return false;
+        }
+    }
+    
+    public async Task<bool> SendPhotoAsync(SendPhotoArgs sendPhotoArgs)
+    {
+        try
+        {
+            await this.BotClient.SendPhotoAsync(sendPhotoArgs);
             return true;
         }
         catch (Exception e)
