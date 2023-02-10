91b70765eb48fe57425965fa75323ded293981d8
Yuri Ratkevich <ura_rat@mail.ru>
2023-02-10 14:49:59 +0300

    Add pool and mediaGroups validation on sayall command, transfer user model to core

---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+
+namespace TelegramBot_Timetable_Core.Models
+{
+    public class User
+    {
+        public ObjectId Id { get; set; }
+        public long UserId { get; set; }
+        public string? Username { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Teacher { get; set; }
+        public bool Notifications { get; set; }
+
+        public User(long userId, string? username, string? firstName, string? lastName)
+        {
+            this.UserId = userId;
+            this.Username = username;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+    }
+}
--- Services/BotService.cs
+++ Services/BotService.cs
@@ -1,6 +1,10 @@
-﻿using Telegram.BotAPI;
+﻿using System.Text.RegularExpressions;
+using MongoDB.Driver;
+using Telegram.BotAPI;
 using Telegram.BotAPI.AvailableMethods;
+using Telegram.BotAPI.AvailableTypes;
 using TelegramBot_Timetable_Core.Config;
+using User = TelegramBot_Timetable_Core.Models.User;
 
 namespace TelegramBot_Timetable_Core.Services;
 
@@ -11,14 +15,19 @@ public interface IBotService
     Task<bool> SendMessageAsync(SendMessageArgs sendMessageArgs);
     bool SendPhoto(SendPhotoArgs sendPhotoArgs);
     Task<bool> SendPhotoAsync(SendPhotoArgs sendPhotoArgs);
+    Task NotifyAllUsers(Message message);
 }
 
 public class BotService : IBotService
 {
+    private readonly IMongoService _mongoService;
+    private Dictionary<string, List<PhotoSize>> _photos = new();
+    private static readonly Regex SayRE = new(@"\/sayall(.+)", RegexOptions.Compiled);
     public BotClient BotClient { get; set; }
 
-    public BotService(IConfig<MainConfig> config)
+    public BotService(IConfig<MainConfig> config, IMongoService mongoService)
     {
+        this._mongoService = mongoService;
         BotClient = new BotClient(config.Entries.Token);
     }
 
@@ -78,4 +87,81 @@ public class BotService : IBotService
             return false;
         }
     }
+    
+    public async Task NotifyAllUsers(Message message)
+        {
+            if (message.MediaGroupId is not null)
+            {
+                if (this._photos.TryGetValue(message.MediaGroupId, out var images))
+                {
+                    if (message.Photo is not null) images.Add(message.Photo.First());
+                }
+                else
+                {
+                    if (message.Photo is not null) this._photos.Add(message.MediaGroupId, new List<PhotoSize>()
+                    {
+                        message.Photo.First()
+                    });
+                }
+            }
+
+            var (result, messageText) = ValidationAllRegexNotification(message);
+            if (!result && message.Poll is null) return;
+
+            var userCollection = this._mongoService.Database.GetCollection<User>("Users");
+            var users = (await userCollection.FindAsync(u => true)).ToList();
+            if (users is null || users.Count <= 0) return;
+            
+            await Task.Delay(2000);
+            
+            var tasks = new List<Task>();
+            List<InputMediaPhoto> mediaPhotos = new();
+
+            if (message.MediaGroupId is not null)
+            {
+                foreach (var p in this._photos[message.MediaGroupId])
+                {
+                    mediaPhotos.Add(new InputMediaPhoto(p.FileId));
+                }
+            }
+
+            
+            foreach (var user in users)
+            {
+                tasks.Add(this.SendMessageAsync(new SendMessageArgs(user.UserId, $"📙Рассылка от бота📙: {messageText}")));
+                try
+                {
+                    if (mediaPhotos.Count > 0) tasks.Add(this.BotClient.SendMediaGroupAsync(new SendMediaGroupArgs(user.UserId, mediaPhotos)));
+                    
+                    if (message.Poll is not null && message.From is not null)
+                    {
+                        tasks.Add(this.BotClient.ForwardMessageAsync(user.UserId, message.From.Id, 
+                            message.MessageId));
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+            
+            await Task.WhenAll(tasks);
+            if (message.MediaGroupId is not null) this._photos[message.MediaGroupId].Clear();
+        }
+
+        private (bool result, string? messageText) ValidationAllRegexNotification(Message message)
+        {
+            var sayRegex = Match.Empty;
+
+            if (message.Text is { } messageText)
+            {
+                sayRegex = SayRE.Match(messageText);
+            }
+            else if (message.Caption is { } msgCaption)
+            {
+                sayRegex = SayRE.Match(msgCaption);
+            }
+
+            return (sayRegex.Length > 0, sayRegex.Groups[1].Value.Trim());
+        }
 }
