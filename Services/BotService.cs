99d60d2b238e65b9555b575db8c9d3a9dfa206ba
Yuri Ratkevich <ura_rat@mail.ru>
2023-02-10 15:27:15 +0300

    Revert sayall in core commit, core update

--- Services/BotService.cs
+++ Services/BotService.cs
@@ -1,10 +1,6 @@
-﻿using System.Text.RegularExpressions;
-using MongoDB.Driver;
-using Telegram.BotAPI;
+﻿using Telegram.BotAPI;
 using Telegram.BotAPI.AvailableMethods;
-using Telegram.BotAPI.AvailableTypes;
 using TelegramBot_Timetable_Core.Config;
-using TelegramBot_Timetable_Core.Models;
 
 namespace TelegramBot_Timetable_Core.Services;
 
@@ -15,19 +11,14 @@ public interface IBotService
     Task<bool> SendMessageAsync(SendMessageArgs sendMessageArgs);
     bool SendPhoto(SendPhotoArgs sendPhotoArgs);
     Task<bool> SendPhotoAsync(SendPhotoArgs sendPhotoArgs);
-    Task NotifyAllUsers(Message message);
 }
 
 public class BotService : IBotService
 {
-    private readonly IMongoService _mongoService;
-    private Dictionary<string, List<PhotoSize>> _photos = new();
-    private static readonly Regex SayRE = new(@"\/sayall(.+)", RegexOptions.Compiled);
     public BotClient BotClient { get; set; }
 
-    public BotService(IConfig<MainConfig> config, IMongoService mongoService)
+    public BotService(IConfig<MainConfig> config)
     {
-        this._mongoService = mongoService;
         BotClient = new BotClient(config.Entries.Token);
     }
 
@@ -87,81 +78,4 @@ public class BotService : IBotService
             return false;
         }
     }
-    
-    public async Task NotifyAllUsers(Message message)
-        {
-            if (message.MediaGroupId is not null)
-            {
-                if (this._photos.TryGetValue(message.MediaGroupId, out var images))
-                {
-                    if (message.Photo is not null) images.Add(message.Photo.First());
-                }
-                else
-                {
-                    if (message.Photo is not null) this._photos.Add(message.MediaGroupId, new List<PhotoSize>()
-                    {
-                        message.Photo.First()
-                    });
-                }
-            }
-
-            var (result, messageText) = ValidationAllRegexNotification(message);
-            if (!result && message.Poll is null) return;
-
-            var userCollection = this._mongoService.Database.GetCollection<TeachersUser>("Users");
-            var users = (await userCollection.FindAsync(u => true)).ToList();
-            if (users is null || users.Count <= 0) return;
-            
-            await Task.Delay(2000);
-            
-            var tasks = new List<Task>();
-            List<InputMediaPhoto> mediaPhotos = new();
-
-            if (message.MediaGroupId is not null)
-            {
-                foreach (var p in this._photos[message.MediaGroupId])
-                {
-                    mediaPhotos.Add(new InputMediaPhoto(p.FileId));
-                }
-            }
-
-            
-            foreach (var user in users)
-            {
-                tasks.Add(this.SendMessageAsync(new SendMessageArgs(user.UserId, $"📙Рассылка от бота📙: {messageText}")));
-                try
-                {
-                    if (mediaPhotos.Count > 0) tasks.Add(this.BotClient.SendMediaGroupAsync(new SendMediaGroupArgs(user.UserId, mediaPhotos)));
-                    
-                    if (message.Poll is not null && message.From is not null)
-                    {
-                        tasks.Add(this.BotClient.ForwardMessageAsync(user.UserId, message.From.Id, 
-                            message.MessageId));
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                }
-            }
-            
-            await Task.WhenAll(tasks);
-            if (message.MediaGroupId is not null) this._photos[message.MediaGroupId].Clear();
-        }
-
-        private (bool result, string? messageText) ValidationAllRegexNotification(Message message)
-        {
-            var sayRegex = Match.Empty;
-
-            if (message.Text is { } messageText)
-            {
-                sayRegex = SayRE.Match(messageText);
-            }
-            else if (message.Caption is { } msgCaption)
-            {
-                sayRegex = SayRE.Match(msgCaption);
-            }
-
-            return (sayRegex.Length > 0, sayRegex.Groups[1].Value.Trim());
-        }
 }
