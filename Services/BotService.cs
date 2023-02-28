f9bc85ad20d5d05339c2124b8ed4f627c66a830d
Yuri Ratkevich <ura_rat@mail.ru>
2023-02-28 19:26:06 +0300

    Add chunk system to send message method

--- Services/BotService.cs
+++ Services/BotService.cs
@@ -13,8 +13,12 @@ public interface IBotService
     Task<bool> SendMessageAsync(SendMessageArgs sendMessageArgs);
     bool SendPhoto(SendPhotoArgs sendPhotoArgs);
     Task<bool> SendPhotoAsync(SendPhotoArgs sendPhotoArgs);
-    bool EditMessageText(long chatId, int messageId, string text, string parseMode = null, MessageEntity[] entities = null);
-    Task<bool> EditMessageTextAsync(long chatId, int messageId, string text, string parseMode = null, MessageEntity[] entities = null);
+
+    bool EditMessageText(long chatId, int messageId, string text, string parseMode = null,
+        MessageEntity[] entities = null);
+
+    Task<bool> EditMessageTextAsync(long chatId, int messageId, string text, string parseMode = null,
+        MessageEntity[] entities = null);
 }
 
 public class BotService : IBotService
@@ -31,7 +35,26 @@ public class BotService : IBotService
     {
         try
         {
-            this.BotClient.SendMessage(sendMessageArgs);
+            int chunkSize = 4000;
+            
+            if (sendMessageArgs.Text.Length > chunkSize)
+            {
+                int strLength = sendMessageArgs.Text.Length;
+                for (int i = 0; i < strLength; i += chunkSize)
+                {
+                    if (i + chunkSize > strLength) chunkSize = strLength - i;
+
+                    this.BotClient.SendMessage((long)sendMessageArgs.ChatId,
+                        sendMessageArgs.Text.Substring(i, chunkSize), sendMessageArgs.MessageThreadId,
+                        sendMessageArgs.ParseMode,
+                        sendMessageArgs.Entities, sendMessageArgs.DisableWebPagePreview,
+                        sendMessageArgs.DisableNotification, sendMessageArgs.ProtectContent,
+                        sendMessageArgs.ReplyToMessageId, sendMessageArgs.AllowSendingWithoutReply,
+                        sendMessageArgs.ReplyMarkup);
+                }
+            }
+            else this.BotClient.SendMessage(sendMessageArgs);
+
             return true;
         }
         catch (Exception e)
@@ -41,7 +64,41 @@ public class BotService : IBotService
         }
     }
     
-    public bool EditMessageText(long chatId, int messageId, string text, string parseMode = null, MessageEntity[] entities = null)
+    public async Task<bool> SendMessageAsync(SendMessageArgs sendMessageArgs)
+    {
+        try
+        {
+            int chunkSize = 4000;
+            
+            if (sendMessageArgs.Text.Length > chunkSize)
+            {
+                int strLength = sendMessageArgs.Text.Length;
+                for (int i = 0; i < strLength; i += chunkSize)
+                {
+                    if (i + chunkSize > strLength) chunkSize = strLength - i;
+
+                    await this.BotClient.SendMessageAsync((long)sendMessageArgs.ChatId,
+                        sendMessageArgs.Text.Substring(i, chunkSize), sendMessageArgs.MessageThreadId,
+                        sendMessageArgs.ParseMode,
+                        sendMessageArgs.Entities, sendMessageArgs.DisableWebPagePreview,
+                        sendMessageArgs.DisableNotification, sendMessageArgs.ProtectContent,
+                        sendMessageArgs.ReplyToMessageId, sendMessageArgs.AllowSendingWithoutReply,
+                        sendMessageArgs.ReplyMarkup);
+                }
+            }
+            else this.BotClient.SendMessage(sendMessageArgs);
+
+            return true;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return false;
+        }
+    }
+
+    public bool EditMessageText(long chatId, int messageId, string text, string parseMode = null,
+        MessageEntity[] entities = null)
     {
         try
         {
@@ -54,8 +111,9 @@ public class BotService : IBotService
             return false;
         }
     }
-    
-    public async Task<bool> EditMessageTextAsync(long chatId, int messageId, string text, string parseMode = null, MessageEntity[] entities = null)
+
+    public async Task<bool> EditMessageTextAsync(long chatId, int messageId, string text, string parseMode = null,
+        MessageEntity[] entities = null)
     {
         try
         {
@@ -69,20 +127,6 @@ public class BotService : IBotService
         }
     }
 
-    public async Task<bool> SendMessageAsync(SendMessageArgs sendMessageArgs)
-    {
-        try
-        {
-            await this.BotClient.SendMessageAsync(sendMessageArgs);
-            return true;
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
-            return false;
-        }
-    }
-    
     public bool SendPhoto(SendPhotoArgs sendPhotoArgs)
     {
         try
@@ -96,7 +140,7 @@ public class BotService : IBotService
             return false;
         }
     }
-    
+
     public async Task<bool> SendPhotoAsync(SendPhotoArgs sendPhotoArgs)
     {
         try
