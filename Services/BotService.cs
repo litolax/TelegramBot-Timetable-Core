97c130f7212807db322bc638fd298449fdbde8e4
litolax <yuri.ratkevich85360@gmail.com>
2023-09-01 01:46:27 +0300

    Add API to send admin messages

--- Services/BotService.cs
+++ Services/BotService.cs
@@ -19,14 +19,19 @@ public interface IBotService
 
     Task<bool> EditMessageTextAsync(long chatId, int messageId, string text, string parseMode = null,
         MessageEntity[] entities = null);
+
+    bool SendAdminMessage(SendMessageArgs sendMessageArgs);
+    Task<bool> SendAdminMessageAsync(SendMessageArgs sendMessageArgs);
 }
 
 public class BotService : IBotService
 {
+    private readonly IConfig<MainConfig> _config;
     public BotClient BotClient { get; set; }
 
     public BotService(IConfig<MainConfig> config)
     {
+        this._config = config;
         this.BotClient = new BotClient(config.Entries.Token);
     }
 
@@ -36,7 +41,7 @@ public class BotService : IBotService
         try
         {
             int chunkSize = 4000;
-            
+
             if (sendMessageArgs.Text.Length > chunkSize)
             {
                 int strLength = sendMessageArgs.Text.Length;
@@ -63,13 +68,63 @@ public class BotService : IBotService
             return false;
         }
     }
-    
+
+    public bool SendAdminMessage(SendMessageArgs sendMessageArgs)
+    {
+        if (this._config.Entries.Administrators is null) return false;
+        try
+        {
+            int chunkSize = 4000;
+
+            if (sendMessageArgs.Text.Length > chunkSize)
+            {
+                int strLength = sendMessageArgs.Text.Length;
+                for (int i = 0; i < strLength; i += chunkSize)
+                {
+                    if (i + chunkSize > strLength) chunkSize = strLength - i;
+
+                    foreach (var administrator in this._config.Entries.Administrators)
+                    {
+                        this.BotClient.SendMessage(administrator,
+                            sendMessageArgs.Text.Substring(i, chunkSize), sendMessageArgs.MessageThreadId,
+                            sendMessageArgs.ParseMode,
+                            sendMessageArgs.Entities, sendMessageArgs.DisableWebPagePreview,
+                            sendMessageArgs.DisableNotification, sendMessageArgs.ProtectContent,
+                            sendMessageArgs.ReplyToMessageId, sendMessageArgs.AllowSendingWithoutReply,
+                            sendMessageArgs.ReplyMarkup);
+                    }
+                }
+            }
+            else
+            {
+                foreach (var administrator in this._config.Entries.Administrators)
+                {
+                    this.BotClient.SendMessage(administrator,
+                        sendMessageArgs.Text,
+                        sendMessageArgs.MessageThreadId,
+                        sendMessageArgs.ParseMode,
+                        sendMessageArgs.Entities, sendMessageArgs.DisableWebPagePreview,
+                        sendMessageArgs.DisableNotification, sendMessageArgs.ProtectContent,
+                        sendMessageArgs.ReplyToMessageId, sendMessageArgs.AllowSendingWithoutReply,
+                        sendMessageArgs.ReplyMarkup);
+                }
+            }
+
+            return true;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return false;
+        }
+    }
+
     public async Task<bool> SendMessageAsync(SendMessageArgs sendMessageArgs)
     {
         try
         {
             int chunkSize = 4000;
-            
+
             if (sendMessageArgs.Text.Length > chunkSize)
             {
                 int strLength = sendMessageArgs.Text.Length;
@@ -86,7 +141,57 @@ public class BotService : IBotService
                         sendMessageArgs.ReplyMarkup);
                 }
             }
-            else this.BotClient.SendMessage(sendMessageArgs);
+            else await this.BotClient.SendMessageAsync(sendMessageArgs);
+
+            return true;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return false;
+        }
+    }
+
+    public async Task<bool> SendAdminMessageAsync(SendMessageArgs sendMessageArgs)
+    {
+        if (this._config.Entries.Administrators is null) return false;
+        try
+        {
+            int chunkSize = 4000;
+
+            if (sendMessageArgs.Text.Length > chunkSize)
+            {
+                int strLength = sendMessageArgs.Text.Length;
+                for (int i = 0; i < strLength; i += chunkSize)
+                {
+                    if (i + chunkSize > strLength) chunkSize = strLength - i;
+
+                    foreach (var administrator in this._config.Entries.Administrators)
+                    {
+                        await this.BotClient.SendMessageAsync(administrator,
+                            sendMessageArgs.Text.Substring(i, chunkSize), sendMessageArgs.MessageThreadId,
+                            sendMessageArgs.ParseMode,
+                            sendMessageArgs.Entities, sendMessageArgs.DisableWebPagePreview,
+                            sendMessageArgs.DisableNotification, sendMessageArgs.ProtectContent,
+                            sendMessageArgs.ReplyToMessageId, sendMessageArgs.AllowSendingWithoutReply,
+                            sendMessageArgs.ReplyMarkup);
+                    }
+                }
+            }
+            else
+            {
+                foreach (var administrator in this._config.Entries.Administrators)
+                {
+                    await this.BotClient.SendMessageAsync(administrator,
+                        sendMessageArgs.Text,
+                        sendMessageArgs.MessageThreadId,
+                        sendMessageArgs.ParseMode,
+                        sendMessageArgs.Entities, sendMessageArgs.DisableWebPagePreview,
+                        sendMessageArgs.DisableNotification, sendMessageArgs.ProtectContent,
+                        sendMessageArgs.ReplyToMessageId, sendMessageArgs.AllowSendingWithoutReply,
+                        sendMessageArgs.ReplyMarkup);
+                }
+            }
 
             return true;
         }
